73ca71873cafd53167e254f71816e8159098ee44
kjknisely <40246011+kjknisely@users.noreply.github.com>
2019-04-10 10:08:43 -0500

    Add files via upload
    initial table create sql

---
+++ SAKAutoAuction.sql
@@ -0,0 +1,44 @@
+create table AuctionAuto (
+	vin varchar(13) primary key,
+	color varchar(10),
+	make varchar(16),
+	model varchar(16),
+	owner varchar(32),
+	year int,
+	title char,
+	trans char,
+	foreign key (lot_num) references location (lot_number),
+	reserve int,
+	mileage long,
+);
+
+create table Location (
+	num_cars_in_lot int,
+	rent long,
+	lot_size int,
+	l_address varchar(100),
+	lot_number int primary key,
+);
+
+create table Employee (
+	ssn int primary key,
+	name varchar(30),
+	address varchar(100),
+	salary long,
+	foreign key (e_lot_num) references location (lot_number),
+);
+
+create table Auctioneer (
+	foreign key (assn) references Employee (ssn),
+	sales_rate int,
+);
+
+create table BidRep(
+	foreign key (bssn) references Employee (ssn),
+	numsales int,
+);
+
+create table Driver (
+	foreign key (bssn) references Employee (ssn),
+	License_num varchar(20),
+);
