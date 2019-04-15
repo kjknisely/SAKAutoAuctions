2be1580be5e56e1cd6bbefb3c5ba0ea9824e90da
kjknisely <40246011+kjknisely@users.noreply.github.com>
2019-04-15 12:32:02 -0500

    upload locations and cleaned up autos. #cars in lot == auto foreign key count

---
+++ Location.sql
@@ -0,0 +1,12 @@
+create table Location (
+	Nnum_cars_in_lot INT,
+	rent int,
+	lot_size INT,
+	l_address VARCHAR(50),
+	lot_number INT primary key
+);
+insert into Location (Nnum_cars_in_lot, rent, lot_size, l_address, lot_number) values (121, 107279.06, 397, '29 Petterle Street', 3);
+insert into Location (Nnum_cars_in_lot, rent, lot_size, l_address, lot_number) values (93, 188844.50, 272, '6000 Melvin Crossing', 2);
+insert into Location (Nnum_cars_in_lot, rent, lot_size, l_address, lot_number) values (95, 326986.15, 237, '5 Surrey Junction', 1);
+insert into Location (Nnum_cars_in_lot, rent, lot_size, l_address, lot_number) values (89, 184719.62, 113, '52 Stone Corner Pass', 5);
+insert into Location (Nnum_cars_in_lot, rent, lot_size, l_address, lot_number) values (102, 329185.51, 196, '07 Warner Lane', 4);
