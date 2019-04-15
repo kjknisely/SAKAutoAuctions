27bec8df3a5a47b5b07517b369d56fd0661d09d6
kjknisely <40246011+kjknisely@users.noreply.github.com>
2019-04-15 12:35:38 -0500

    employee tables

---
+++ Employee.sql
@@ -0,0 +1,59 @@
+create table Employee (
+	ssn int,
+	salary INT,
+	name VARCHAR(50),
+	address VARCHAR(50),
+	e_lot_number INT,
+	primary key (ssn),
+	foreign key (e_lot_number) references location (lot_number)
+);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('344342341', 74360, 'Tobe Ricketts', '18 Lighthouse Bay Parkway', 5);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('192496284', 107232, 'Irvin Cheale', '683 Bayside Park', 5);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('290598779', 51646, 'Marilee Livingstone', '98 Florence Pass', 1);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('845254654', 37220, 'Madelin Pelzer', '28840 Clemons Court', 5);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('323771018', 52468, 'Abagael Dussy', '06 Burrows Avenue', 2);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('339659428', 101965, 'Dominick Rouch', '7673 Milwaukee Alley', 2);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('353024433', 64901, 'Susana Raff', '1257 Homewood Avenue', 2);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('856788069', 28883, 'Sorcha Lorraine', '6710 Vernon Plaza', 2);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('569961477', 82604, 'Evie Devers', '75 Weeping Birch Pass', 3);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('936044654', 33558, 'Bethena Siddele', '693 Barby Terrace', 1);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('456506149', 118598, 'Spenser Rikel', '61 Superior Way', 5);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('407895301', 90446, 'Tobey Bundey', '03 Truax Point', 1);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('563248510', 26756, 'Delbert Jeromson', '75 Heath Way', 1);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('631899888', 61622, 'Florette Bromage', '6480 Atwood Plaza', 3);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('993216166', 34802, 'Wash Perrot', '74021 Veith Lane', 5);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('663872597', 30210, 'Anjanette McCumesky', '8027 Southridge Place', 4);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('107909201', 70425, 'Mirabelle Hourston', '41518 Kenwood Trail', 3);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('746749350', 104323, 'Alexa Petigrew', '81610 Oriole Way', 4);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('760597127', 88250, 'Torrey Hauxwell', '11 Dennis Avenue', 2);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('346259342', 90209, 'Florrie Lewty', '3408 Transport Point', 3);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('867421136', 36016, 'Dugald Dalby', '752 Almo Drive', 5);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('008795377', 118108, 'Missy Bednell', '39527 Lukken Pass', 5);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('061190266', 82759, 'Lilias Shepperd', '36950 Eastwood Hill', 1);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('584277178', 103515, 'Perri Wadly', '70536 Express Terrace', 2);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('219288487', 31678, 'Aylmar Fourmy', '764 Valley Edge Crossing', 1);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('060751965', 77844, 'Phebe Caban', '63286 Hanover Junction', 3);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('501805751', 52545, 'Nariko Gilley', '9 Milwaukee Parkway', 4);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('796328349', 49501, 'Walker Crumly', '235 Hoffman Plaza', 1);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('066257142', 42110, 'Berthe Gammade', '47262 Donald Way', 4);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('802443958', 111054, 'Sampson Gilling', '06 Rusk Center', 2);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('984159086', 117528, 'Wyatan Merrifield', '38 American Ash Lane', 5);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('737635511', 124468, 'Gerrie Kinloch', '4 Emmet Crossing', 1);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('352308146', 29045, 'Addie Champkin', '14918 Huxley Circle', 1);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('096606700', 120547, 'Shannen Plascott', '30426 Parkside Drive', 4);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('486491278', 34071, 'Luigi Bissett', '744 Dunning Court', 1);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('718906354', 121235, 'Francene Benkhe', '292 Erie Trail', 3);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('207347219', 78349, 'Pierson Witcherley', '15574 Dapin Road', 3);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('894085150', 98737, 'Dallas Beden', '02044 Meadow Valley Point', 4);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('026450617', 85243, 'Grannie McVrone', '4924 Vermont Terrace', 5);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('156743744', 66714, 'Lacee Coupar', '7 Westridge Point', 4);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('926712707', 90216, 'Yvon Grinnikov', '43158 Riverside Circle', 5);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('017320864', 104551, 'Dulcy Dreamer', '084 Tony Plaza', 3);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('588300301', 118231, 'Jorey Gronowe', '36 Hazelcrest Pass', 3);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('628761508', 121095, 'Garnet Hourahan', '82645 Springview Pass', 2);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('187413366', 28376, 'Betteanne Mogenot', '65 Union Parkway', 3);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('616166991', 28712, 'Tabby Vlies', '25 Caliangt Place', 4);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('693872023', 25176, 'Devonne Sussans', '40 Fisk Parkway', 5);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('470586541', 78915, 'Wylie Oblein', '2 Stuart Drive', 2);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('541848673', 118668, 'Alexa Coweuppe', '4 Spohn Center', 1);
+insert into Employee (ssn, salary, name, address, e_lot_number) values ('141462261', 25074, 'Allianora Oldall', '0393 Browning Trail', 2);
